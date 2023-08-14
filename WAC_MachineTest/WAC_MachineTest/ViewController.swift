9be140a2dbb19a82058a25d30d909bab1c67a90b
Chandramohan <chandra@qaptive.co.in>
2023-08-14 06:52:47 +0530

    initial project setups

---
+++ WAC_MachineTest/WAC_MachineTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WAC_MachineTest
+//
+//  Created by Chandra Mohan on 14/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
